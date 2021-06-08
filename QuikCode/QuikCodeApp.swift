652edd607822cb8f4ef2fc97fe2ad2176a4e4863
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-06-08 20:03:32 +0100

    Initial Commit

---
+++ QuikCode/QuikCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuikCodeApp.swift
+//  QuikCode
+//
+//  Created by Conor on 08/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuikCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
